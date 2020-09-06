0bef754d2512f734c6fc5e77bb3075f2447a1c13
HoangVy <hoangvynguyen42@gmail.com>
2020-09-06 11:33:06 +0700

    update cors in server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var router = require("./routers/index");
+var cors = require("cors");
 
 app.use(
   bodyParser.urlencoded({
@@ -11,6 +12,25 @@ app.use(
 app.use(bodyParser.json());
 app.use("/", router);
 app.use(express.json());
+
+// pass CORS
+app.use((req, res, next) => {
+  res.header("Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE"
+  );
+  req.header("Access-Control-Allow-Origin", "*");
+
+  next();
+});
+app.use(cors());
+
 app.listen(process.env.PORT || 8081, function () {
   console.log("server running on port 8081!");
 });
